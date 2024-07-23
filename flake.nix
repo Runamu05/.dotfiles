45c6419c7d7c1d818583124a278d87267b54e2f2
Runamu05 <marmanuel4@gmail.com>
2024-07-23 20:37:36 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -1,42 +1,50 @@
 {
-    description = "My Flake";
+  description = "My Flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-	home-manager = {
-	  url = "github:nix-community/home-manager";
-	  inputs.nixpkgs.follows = "nixpkgs";
-	};
+    home-manager = {
+	    url = "github:nix-community/home-manager";
+	    inputs.nixpkgs.follows = "nixpkgs";
+	  };
+
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, home-manager, ... }@inputs: let
-    	inherit (self) outputs;
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+    inherit (self) outputs;
 
-    	system = "x86_64-linux";
-        lib = nixpkgs.lib;
-        pkgs = import nixpkgs {inherit system;};
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+    pkgs = import nixpkgs { inherit system; };
     in {
-	nixosConfigurations = {
-	    system = lib.nixosSystem {
-	    	inherit system;
-		modules = [ ./configuration.nix ];
-		specialArgs = {
-		    inherit inputs;
-		    inherit outputs;
-		};
+	    nixosConfigurations = {
+	      system = lib.nixosSystem {
+	    	  inherit system;
+		      modules = [ ./profiles/base/configuration.nix ];
+		      specialArgs = {
+		        inherit inputs;
+		        inherit outputs;
+		      };
+	      };
 	    };
-	};
 
-	homeConfigurations = {
-	    user = home-manager.lib.homeManagerConfiguration {
-		inherit pkgs;
-		modules = [ ./home.nix ];
-		extraSpecialArgs = {
-		    inherit inputs;
-		    inherit outputs;
-		};
+	    homeConfigurations = {
+	      user = home-manager.lib.homeManagerConfiguration {
+		      inherit pkgs;
+		      extraSpecialArgs = {
+		        inherit inputs;
+		        inherit outputs;
+		      };
+         	modules = [
+            ./profiles/base/home.nix
+          ]; 
+	      };
 	    };
-	};
     };
-}
+  }
+
