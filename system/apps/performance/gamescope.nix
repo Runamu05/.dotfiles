7b25b24c119a6209446aff789ca1a40f012aacc2
Runamu05 <marmanuel4@gmail.com>
2024-08-19 21:24:28 +0200

    added gamescope

---
+++ system/apps/performance/gamescope.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  programs.gamescope = {
+    enable = true;
+    env = {
+      "XKB_DEFAULT_LAYOUT" = "de";
+      "XKB_DEFAULT_VARIANT" = "nodeadkeys";
+      "-W" = "1920";
+      "-w" = "1920";
+      "-H" = "1080";
+      "-h" = "1080";
+      "-r" = "60";
+      "--backend" = "wayland";
+    };
+    args = [
+      "-f"
+      "-e"
+      "--force-windows-fullscreen"
+      "--mangoapp"
+    ];
+  };
+}
