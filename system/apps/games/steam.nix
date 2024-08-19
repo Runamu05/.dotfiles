8b40d5b3dcd8dde82cdea045fae23d39422e4b31
Runamu05 <marmanuel4@gmail.com>
2024-08-19 20:23:35 +0200

    moved gamescope and gamemode

--- system/apps/games/steam.nix
+++ system/apps/games/steam.nix
@@ -6,48 +6,11 @@
     gamescopeSession.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [ mangohud protonup ];
+  environment.systemPackages = with pkgs; [ protonup ];
 
   environment.sessionVariables = {
      STEAM_EXTRA_COMPAT_TOOLS_PATHS =
         "/home/runamu/.steam/root/compatibilitytools.d";
   };
-
-  programs.gamescope = {
-    enable = true;
-    env = {
-      "XKB_DEFAULT_LAYOUT" = "de";
-      "XKB_DEFAULT_VARIANT" = "nodeadkeys";
-      "-W" = "1920";
-      "-H" = "1080";
-      "-w" = "1920";
-      "-h" = "1080";
-      "-r" = "60";
-      "--backend" = "wayland";
-    };
-    args = [
-      "-f"
-      "-e"
-      "--force-windows-fullscreen"
-      "--expose-wayland"
-    ];
-  };
-
-  programs.gamemode = {
-    enable = true;
-    enableRenice = true;
-    settings = {
-      general = {
-        renice = 1;
-        desiredgov = "performance";
-        igpu_desiredgov = "performance";
-      };
-      cpu = {
-        park_cores = false;
-        #pin_cores = "yes";
-      };
-    };
-  };
-
 }
 
