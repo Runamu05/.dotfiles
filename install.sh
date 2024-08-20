952145643c7be887ce3f676d2f16fe5576e4cc81
Runamu05 <marmanuel4@gmail.com>
2024-08-20 06:44:55 +0200

    Install my NixOS Config

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo nixos-generate-config --show-hardware-config > ./system/hardware-configuration.nix
+
+sudo nixos-rebuild switch --flake ~/.dotfiles#system
+
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#user
