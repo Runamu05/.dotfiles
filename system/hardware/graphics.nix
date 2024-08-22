4388ad86755f09b3c0325b3bb195407e51f1991f
Runamu05 <runamu05@gmail.com>
2024-08-22 20:34:15 +0200

    updated syntax

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 
 {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-
-    #extraPackages = [ pkgs.amdvlk ];
-    #extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    amdgpu.amdvlk = {
+      enable = true;
+      #support32Bit = "pkgs.driversi686Linux.amdvlk";
+      #supportExperimental = true;
+    };
   };
 
   services.xserver.videoDrivers = ["amdgpu"];
