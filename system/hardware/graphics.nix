6431c63303ec582881bed862dcf83fb82e74a80c
Runamu05 <marmanuel4@gmail.com>
2024-08-22 03:23:56 +0200

    commented drivers

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -5,8 +5,8 @@
     enable = true;
     enable32Bit = true;
 
-    extraPackages = [ pkgs.amdvlk ];
-    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+    #extraPackages = [ pkgs.amdvlk ];
+    #extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
   };
 
   services.xserver.videoDrivers = ["amdgpu"];
