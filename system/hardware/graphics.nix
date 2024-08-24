9bff52bfd66b02035ff4a238ff1032f8ff6c5672
Runamu05 <runamu05@gmail.com>
2024-08-24 22:05:58 +0200

    re-added drivers

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -5,12 +5,14 @@
     graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = [ pkgs.amdvlk ];
+      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };
-    amdgpu.amdvlk = {
-      enable = true;
-      #support32Bit = "pkgs.driversi686Linux.amdvlk";
-      #supportExperimental = true;
-    };
+    #amdgpu.amdvlk = {
+    # enable = true;
+    # support32Bit = "pkgs.driversi686Linux.amdvlk";
+    # supportExperimental = true;
+    #};
   };
 
   services.xserver.videoDrivers = ["amdgpu"];
