65d7045cd10e739d6ca8570b14f2f1bf7bb14738
Runamu05 <runamu05@gmail.com>
2024-08-25 08:11:28 +0200

    changed

--- user/shell/default.nix
+++ user/shell/default.nix
@@ -2,8 +2,11 @@
 
 {
   home.packages = with pkgs; [
+    # Download manager via HTTPS, HTTP, FTP
     wget
+    # Transfer data via URL syntax
     curl
+    # Get general system info
     neofetch
     # Get system stats
     htop
