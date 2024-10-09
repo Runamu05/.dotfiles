5c4876780f8340f1e525b44fedfd105ec667c552
Runamu05 <runamu05@gmail.com>
2024-10-09 15:42:48 +0200

    Added Heroic Launcher

--- user/apps/games.nix
+++ user/apps/games.nix
@@ -4,7 +4,11 @@
   home.packages = with pkgs; [
     # Wii U Emulator
     cemu
+    # BoTW Mod Manager
+    ukmm
     # Mod manager and launcher for Minecraft Premium
     prismlauncher
+    # Native GOG, Epic, and Amazon Games Launcher 
+    heroic
   ];
 }
